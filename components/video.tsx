733dd3f0af67fa674d1dac7ac696dede91716d3c
Andy <hiromansofaoi@gmail.com>
2024-08-23 14:44:37 +0000

    p

--- components/video.tsx
+++ components/video.tsx
@@ -1,6 +1,4 @@
-const VideoComponent = ({id}:{id:string}) => {
-  // const url = "https://utfs.io/f/1c6d3fd9-d63c-44b4-b27d-c4e5c62264e9-xzpxms.mp4"
-  const url = "https://makevos.com/storage/videos/ef43de.mp4";
+const VideoComponent = ({ id }: any) => {
   return (
     <div
       style={{
@@ -13,21 +11,15 @@ const VideoComponent = ({id}:{id:string}) => {
         padding: "10px",
       }}
     >
-      <div>
-        <video controls style={{ width: "100%", height: "auto" }}>
-          <source src={`https://makevos.com/storage/videos/${id}.mp4`} type="video/mp4" />
-        </video>
-        {/* <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/ly36kn0ug4k?si=bthTCrpntrWM6Axp"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerpolicy="strict-origin-when-cross-origin"
-          allowfullscreen
-        ></iframe> */}
-      </div>
+      <video
+        controls
+        style={{ width: "75%", height: "auto", maxHeight: "80%" }}
+      >
+        <source
+          src={`https://makevos.com/storage/videos/${id}.mp4`}
+          type="video/mp4"
+        />
+      </video>
     </div>
   );
 };
